fbe65630fdaa42c9a6562866be2b9e8847d458a6
IsaacT30 <isaaca.torres@ute.edu.ec>
2025-11-25 02:07:27 -0500

    Update

--- medical_services/serializers/specialty.py
+++ medical_services/serializers/specialty.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
+from django.utils.text import slugify
 from medical_services.models import Specialty
 
 class SpecialtySerializer(serializers.ModelSerializer):
     class Meta:
         model = Specialty
         fields = ("id","name","slug","description","created_at","updated_at")
-        read_only_fields = ("id","created_at","updated_at")
+        read_only_fields = ("id","slug","created_at","updated_at")
+    
+    def create(self, validated_data):
+        # Auto-generar slug desde el nombre si no se proporciona
+        if 'slug' not in validated_data or not validated_data['slug']:
+            validated_data['slug'] = slugify(validated_data['name'])
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        # Auto-generar slug si el nombre cambia
+        if 'name' in validated_data and validated_data['name'] != instance.name:
+            validated_data['slug'] = slugify(validated_data['name'])
+        return super().update(instance, validated_data)
